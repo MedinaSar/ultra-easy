25ea23b1a733f1de7b945fb399321ee315036294
MedinaSar <medinas995@gmail.com>
2014-12-27 10:20:55 +0100

    almost done

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#define SIZE 10
+void newUsernameAndPass();
+void checkUserAndPass();
 void writeORread();
 void writing();
 void reading();
 
 void main()
 {
+	char choice;
+
+	printf("Is it your first time here? (Y/N)\n");
+	scanf("%c", &choice);
+
+	if (choice == 'Y')
+		newUsernameAndPass();
+
+	else if (choice == 'N')
+		checkUserAndPass();
+
 	writeORread();
+
 	system("pause");
 }
-
-
 void writeORread()
 {
 	int choice;
@@ -20,20 +34,62 @@ void writeORread()
 	system("cls");
 
 	if (choice == 1)
-	{
 		writing();
+	else if (choice == 2)
+		reading();
+}
+void newUsernameAndPass()
+{
+	char *username[SIZE];
+	FILE *usernamef = fopen("D://username.txt", "w");
+
+	printf("Enter your username (up to 10 characteres): ");
+	scanf("%s", &username);
+
+	fprintf(usernamef, "%s", username);
+	fclose(usernamef);
+
+	char password[SIZE];
+
+	FILE *passwordf = fopen("D://password.txt", "w");
+
+	printf("\nEnter your password (up to 10 characteres): ");
+	scanf("%s", &password);
+
+	fprintf(passwordf, "%s", password);
+	fclose(passwordf);
+}
+void checkUserAndPass()
+{
+	char usernam[SIZE], password[SIZE], checku[SIZE], checkp[SIZE];
+
+	FILE *username = fopen("D://username.txt", "r");
+	FILE *pasword = fopen("D://password.txt", "r");
+
+	printf("\nEnter your username:");
+	scanf("%s", &usernam);
+
+	printf("\nEnter your password:");
+	scanf("%s", &password);
+
+	fscanf(username, "%s", &checku);
+	fclose(username);
+
+	fscanf(pasword, "%s", &checkp);
+	fclose(pasword);
+
+	if (strcmp(usernam, checku) == 0 && strcmp(password, checkp) == 0) 
+	{
+		printf("\n\n\t\tHello, %s!\n\n", usernam);
 	}
 
-	else if (choice == 2)
-	{
-		reading();
-	}
-}
+	else //keep asking for the user and pass until it's correct!
+		printf("\n\nYour username or password is incorrect.Please check them again.\n\n");
 
 void enteringDate()
 {
 	FILE *diary;
-	diary = fopen("D:\\diary.txt", "a+");
+	diary= fopen("D:\\diary.txt", "a+");
 
 	int day, month, year;
 
@@ -46,13 +102,13 @@ void enteringDate()
 	printf("\nYear: ");
 	scanf("%d", &year);
 
-	fprintf(diary, "\n\n%d.%d.%d.", day, month, year);
-}
+	fprintf(diary, "\n\n%d.%d.%d.\n", day, month, year);
 
+	fclose(diary);
+}
 void writing()
 {
-	FILE *diary;
-	diary = fopen("D:\\diary.txt", "a+");	
+	FILE *diary = fopen("D:\\diary.txt", "a+");	
 
 	enteringDate();
 	printf("\n\nHow was your day?\n");
@@ -70,8 +126,9 @@ void writing()
 
 	printf("\nSeems like a great day!\n\n");
 
-} //I have some problems here, it prints me first the content and then the date. It should be obrnuto.
-
+	_sleep(3000);
+	system("cls");
+} 
 void reading()
 {
 	FILE *diary;
@@ -92,4 +149,7 @@ void reading()
 	printf("%s\n", contents);
 
 	fclose(diary);
+	
+	/*_sleep(5000);
+	system("cls");*/
 }
