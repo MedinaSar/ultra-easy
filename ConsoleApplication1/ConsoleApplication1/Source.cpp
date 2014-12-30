1f3edf3c552a5a4e2519ec0f433f160c14a8529a
MedinaSar <medinas995@gmail.com>
2014-12-30 19:35:45 +0100

    popravljen reading part

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #define SIZE 50
-void newUsernameAndPass();
+
+void newUsername();
+void newPassword();
+void newAccount();
+int checkUser();
+int checkPass();
 void checkUserAndPass();
 void writeORread();
 void writing();
+void enteringDate();
+void dateDay();
+void dateMonth();
+void dateYear();
 void reading();
 
 void main()
@@ -14,106 +23,169 @@ void main()
 	printf("Is it your first time here? (Y/N)\n");
 	scanf("%c", &choice);
 
-	if (choice == 'Y')
-		newUsernameAndPass();
+	if (choice == 'Y' || choice == 'y')
+	{
+		system("cls");
+		newAccount();
+	}
 
-	else if (choice == 'N')
+	else if (choice == 'N' || choice == 'n')
+	{
+		system("cls");
 		checkUserAndPass();
-
-
+	}
 	system("pause");
 }
-void writeORread()
+
+void newUsername()
 {
-	int choice;
-
-	printf("\nWelcome to your diary! Would you like to:\n\n1.write in your diary\n\n2.read from it?\n\n");
-	scanf("%d", &choice);
-
-	system("cls");
-
-	if (choice == 1)
-		writing();
-	else if (choice == 2)
-		reading();
-}
-void newUsernameAndPass()
-{
-	char *username[SIZE];
+	char username[SIZE];
 	FILE *usernamef = fopen("D://username.txt", "w");
 
-	printf("Enter your username (up to 10 characteres): ");
+	printf("Enter your username: ");
 	scanf("%s", &username);
 
 	fprintf(usernamef, "%s", username);
 	fclose(usernamef);
-
+}
+void newPassword()
+{
 	char password[SIZE];
-
 	FILE *passwordf = fopen("D://password.txt", "w");
 
-	printf("\nEnter your password (up to 10 characteres): ");
+	printf("Enter your password: ");
 	scanf("%s", &password);
 
 	fprintf(passwordf, "%s", password);
 	fclose(passwordf);
 }
+void newAccount()
+{
+	newUsername();
+	newPassword();
+
+	printf("\n\nCongratulations! You just created an account!\n\nFeel free to write in your diary whatever pops-up in your mind:\nit won't tell anyone! ;)\n\n");
+	_sleep(6500);
+	system("cls");
+	writeORread();
+}
+
+int checkUser()
+{
+	char username[SIZE], checkUser[SIZE];
+	FILE *usernamef = fopen("D://username.txt", "r");
+
+	printf("Enter your username: ");
+	scanf("%s", &username);
+	fscanf(usernamef, "%s", &checkUser);
+	fclose(usernamef);
+
+	if (strcmp(username, checkUser) == 0)
+		return 0;
+	else
+		return 1;
+}
+int checkPass()
+{
+	char password[SIZE], checkPass[SIZE];
+	FILE *passwordf = fopen("D://password.txt", "r");
+
+	printf("Enter your password: ");
+	scanf("%s", &password);
+	fscanf(passwordf, "%s", &checkPass);
+	fclose(passwordf);
+
+	if (strcmp(password, checkPass) == 0)
+		return 0;
+	else
+		return 1;
+}
 void checkUserAndPass()
 {
-	char usernam[SIZE], password[SIZE], checku[SIZE], checkp[SIZE];
+	while (checkUser() != 0 || checkPass() != 0)
+			printf("\nYour username/password is incorrect. Please check them.\n\n");
+	system("cls");
+	writeORread();
+}
 
-	FILE *username = fopen("D://username.txt", "r");
-	FILE *pasword = fopen("D://password.txt", "r");
+void writeORread()
+{
+	int choice;
 
-	printf("\nEnter your username:");
-	scanf("%s", &usernam);
+	printf("\nHello, there! Welcome to your diary! Would you like to:\n\n1.write in your diary\n2.read from it?\n3.Exit\n\n");
+	scanf("%d", &choice);
+	system("cls");
 
-	printf("\nEnter your password:");
-	scanf("%s", &password);
-
-	fscanf(username, "%s", &checku);
-	fclose(username);
-
-	fscanf(pasword, "%s", &checkp);
-	fclose(pasword);
-
-	if (strcmp(usernam, checku) == 0 && strcmp(password, checkp) == 0)
+	switch (choice)
 	{
-		printf("\n\n\t\tHello, %s!\n\n", usernam);
+	case 1: writing();
 		writeORread();
+	case 2: reading();
+		writeORread();
+	case 3: break;
 	}
-
-	else //keep asking for the user and pass until it's correct!
-		printf("\n\nYour username or password is incorrect.Please check them again.\n\n");
+}
+void dateDay()
+{
+	int day;
+	FILE *diary = fopen("D:\\diary.txt", "a+");
+	while (1)
+	{
+		printf("Day: ");
+		scanf("%d", &day);
+		if (day < 1 || day > 31)
+			printf("\nWrong input. Try again.\n\n");
+		else
+			break;
+	}
+	fprintf(diary,"%d.", day);
+	fclose(diary);
+}
+void dateMonth()
+{
+	int month;
+	FILE *diary= fopen("D:\\diary.txt", "a+");
+	while (1)
+	{
+		printf("Month: ");
+		scanf("%d", &month);
+		if (month < 1 || month > 31)
+			printf("\nWrong input. Try again.\n\n");
+		else
+			break;
+	}
+	fprintf(diary,"%d.", month);
+	fclose(diary);
+}
+void dateYear()
+{
+	int year;
+	FILE *diary = fopen("D:\\diary.txt", "a+");
+	while (1)
+	{
+		printf("Year: ");
+		scanf("%d", &year);
+		if (year < 2015 || year > 2100)
+			printf("\nWrong input. Try again.\n\n");
+		else
+			break;
+	}
+	fprintf(diary, "%d.", year);
+	fclose(diary);
 }
 void enteringDate()
 {
-	FILE *diary;
-	diary= fopen("D:\\diary.txt", "a+");
-
-	int day, month, year;
-
-	printf("Day: ");
-	scanf("%d", &day);
-
-	printf("\nMonth: ");
-	scanf("%d", &month);
-
-	printf("\nYear: ");
-	scanf("%d", &year);
-
-	fprintf(diary, "\n\n%d.%d.%d.\n", day, month, year);
-
-	fclose(diary);
+	dateDay();
+	dateMonth();
+	dateYear();
 }
 void writing()
 {
+	char content;
 	FILE *diary = fopen("D:\\diary.txt", "a+");	
 
 	enteringDate();
 	printf("\n\nHow was your day?\n");
-
-	char content;
 	while (1)
 	{
 		scanf("%c", &content);
@@ -121,34 +193,24 @@ void writing()
 			break;
 		fprintf(diary, "%c", content);
 	}
-
 	fclose(diary);
-
 	printf("\nSeems like a great day!\n\n");
-
-	_sleep(3000);
+	_sleep(2500);
 	system("cls");
 } 
 void reading()
 {
-	FILE *diary;
-	char *contents;
-	int fileSize = 0;
+	char content;
+	FILE *diary = fopen("D:\\diary.txt", "r");
+	fseek(diary, 0, SEEK_SET);
 
-	diary = fopen("D:\\diary.txt", "rb");
-
-	fseek(diary, 0L, SEEK_END);
-	fileSize = ftell(diary);
-	fseek(diary, 0L, SEEK_SET);
-
-	contents = malloc(fileSize + 1);
-
-	size_t size = fread(contents, 1, fileSize, diary);
-	contents[size] = 0; 
-	printf("%s\n", contents);
+	while (!feof(diary))
+	{
+		fscanf(diary, "%c", &content);
+		printf("%c", content);
+	}
 
 	fclose(diary);
-	
-	/*_sleep(5000);
-	system("cls");*/
+	system("pause");
+	system("cls");
 }
